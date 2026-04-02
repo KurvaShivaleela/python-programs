017486bd4c3f9655fc98c6eb65454df516070e49
KurvaShivaleela <kurvashivaleela9701@gmail.com>
2026-04-02 11:11:42 +0530

    Add files via upload

---
+++ addtwonumbers.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def addTwoNumbers(l1, l2):
+    dummy = ListNode(0)
+    current = dummy
+    carry = 0
+    while l1 or l2 or carry:
+        val1 = l1.val if l1 else 0
+        val2 = l2.val if l2 else 0
+
+        total = val1 + val2 + carry
+        carry = total // 10
+
+        current.next = ListNode(total % 10)
+        current = current.next
+
+        if l1:
+            l1 = l1.next
+        if l2:
+            l2 = l2.next
+
+    return dummy.next
+def create_linked_list(arr):
+    dummy = ListNode(0)
+    current = dummy
+    for num in arr:
+        current.next = ListNode(num)
+        current = current.next
+    return dummy.next
+
+
+def linked_list_to_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
+l1 = create_linked_list([2, 4, 3])
+l2 = create_linked_list([5, 6, 4])
+
+result = addTwoNumbers(l1, l2)  
+print(linked_list_to_list(result))  
