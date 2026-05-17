09f5ccc7f4b56bb2780f75548950d06d907ad1fb
KurvaShivaleela <kurvashivaleela9701@gmail.com>
2026-05-17 18:20:47 +0530

    Add files via upload

---
+++ integertorandom.py
@@ -0,0 +1,32 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        ]
+
+        symbols = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        ]
+
+        result = []
+
+        for i in range(len(values)):
+            while num >= values[i]:
+                result.append(symbols[i])
+                num -= values[i]
+
+        return "".join(result)
+
+
+# Create object
+sol = Solution()
+
+# Take input from user
+num = int(input("Enter a number: "))
+
+# Print Roman numeral
+print("Roman Numeral:", sol.intToRoman(num))
